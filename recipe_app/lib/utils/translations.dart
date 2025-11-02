2160960aa93079479304358f7d985dc50dec3b0d
minhleeee123 <157082841+minhleeee123@users.noreply.github.com>
2025-11-02 16:17:32 +0700

    tiếng Anh, tiếng Việt

---
+++ recipe_app/lib/utils/translations.dart
@@ -0,0 +1,12 @@
+final Map<String, Map<String, String>> appStrings = {
+  'vi': {
+    'nav_home': 'Trang Chủ',
+    'nav_favorites': 'Yêu Thích',
+    'home_title': 'Sổ Công Thức',
+  },
+  'en': {
+    'nav_home': 'Home',
+    'nav_favorites': 'Favorites', 
+    'home_title': 'Recipe Book',
+  },
+};
