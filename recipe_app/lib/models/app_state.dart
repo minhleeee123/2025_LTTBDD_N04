728a4f3885080d05bf1330252df8f8d7a776ed8b
minhleeee123 <157082841+minhleeee123@users.noreply.github.com>
2025-11-02 16:18:14 +0700

    tạo app_state

---
+++ recipe_app/lib/models/app_state.dart
@@ -0,0 +1,19 @@
+import 'recipe.dart';
+
+class AppState {
+  static String language = 'vi';
+  static List<Recipe> favoriteRecipes = [];
+  static List<Recipe> allRecipes = List.from(sampleRecipes);
+
+  static bool isFavorite(Recipe recipe) {
+    return favoriteRecipes.any((r) => r.id == recipe.id);
+  }
+
+  static void toggleFavorite(Recipe recipe) {
+    if (isFavorite(recipe)) {
+      favoriteRecipes.removeWhere((r) => r.id == recipe.id);
+    } else {
+      favoriteRecipes.add(recipe);
+    }
+  }
+}
