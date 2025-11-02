92cdbd06556f855fc6a6fd8e776c0b9f49a60411
minhleeee123 <157082841+minhleeee123@users.noreply.github.com>
2025-11-02 16:13:47 +0700

    Basic Navigation

--- recipe_app/lib/main.dart
+++ recipe_app/lib/main.dart
@@ -15,10 +15,37 @@ class RecipeBookApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Recipe Book App'),
-        ),
+      home: MainScreen(), 
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key}); 
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+  
+  final List<Widget> _screens = [
+    const Center(child: Text('Home Screen')), 
+    const Center(child: Text('Favorites')),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favorites'),
+        ],
       ),
     );
   }
